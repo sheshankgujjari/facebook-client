2130dc70771b9a5d9785103de35fd2e90b12418e
Sheshank Gujjari <sheshankgujjari@hotmail.com>
2018-01-08 23:28:58 -0500

    Facebook REST API client

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# facebook-client
+# Facebook-Client
+Rest API using spring
+
+
+##Gradle
+
+Run the application using 
+
+```
+./gradlew bootRun
+```
+
+Build the jar
+
+```
+./gradlew build
+```
+
+Run the Jar after build
+
+```
+java -jar build/libs/facebook-client-1.0.jar
+```
+
+
+
+##Maven
+
+Run the application using 
+
+```
+./mvnw spring-boot:run
+```
+
+Build the JAR file
+
+```
+ ./mvnw clean package
+ ```
+
+
+ Run the jar after maven build
+```
+ java -jar target/facebook-client-1.0.jar
+ ```
+
+
+
+##Run the app
+
+```
+ http://localhost:8080/getUser
+ ```
---
+++ src/main/java/com/facebook/controller/FacebookController.java
@@ -0,0 +1,74 @@
+package com.facebook.controller;
+
+import com.facebook.model.Count;
+import com.restfb.Connection;
+import com.restfb.types.*;
+import com.facebook.helper.FacebookHelper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class FacebookController {
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public User getUser(@RequestHeader String accessToken)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        User user = FacebookHelper.getUserInformation(accessToken);
+        return user;
+    }
+
+    @RequestMapping(value = "/getPosts", method = RequestMethod.GET ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Connection<Post> getPosts(@RequestHeader String accessToken)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        Connection<Post> posts = FacebookHelper.getPosts(accessToken);
+        return posts;
+    }
+
+    @RequestMapping(value = "/getComments/{postId}", method = RequestMethod.GET ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Connection<Comment> getComments(@RequestHeader String accessToken, @PathVariable(name = "postId") String postId)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        Connection<Comment> comments = FacebookHelper.getCommentsByPostId(accessToken, postId);
+        return comments;
+    }
+
+    @RequestMapping(value = "/getCount/{postId}", method = RequestMethod.GET ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Count getCount(@RequestHeader String accessToken, @PathVariable(name = "postId") String postId)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        Count count = FacebookHelper.getCountOfCommentsLikesAndShares(accessToken, postId);
+        return count;
+    }
+
+    @RequestMapping(value = "/getInsights", method = RequestMethod.GET ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Connection<Insight> getCount(@RequestHeader String accessToken)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        Connection<Insight> insights = FacebookHelper.getInsights(accessToken);
+        return insights;
+    }
+
+    @RequestMapping(value = "/publishMessage", method = RequestMethod.POST ,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public GraphResponse publishMessage(@RequestHeader String accessToken, @RequestBody String text)
+    {
+        System.out.println("Get user data");
+        System.out.println("Access Token :" + accessToken);
+        GraphResponse graphResponse = FacebookHelper.publishMessage(accessToken, text);
+        return graphResponse;
+    }
+
+
+}
